817c1119d5fe83535be638ee2dee0b5f6704cf02
REI_PC <svino.gay@mail.ru>
2024-04-21 22:44:51 +0200

    v1_Splash_Icon

---
+++ app/src/main/java/com/example/konigguide/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.konigguide;
+
+import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        GradientDrawable gradientDrawable = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM,
+                new int[]{ContextCompat.getColor(this, R.color.first_fade_pall),
+                        ContextCompat.getColor(this, R.color.second_fade_pall),
+                        ContextCompat.getColor(this, R.color.third_fade_pall),
+                        ContextCompat.getColor(this, R.color.fourth_fade_pall)});
+        findViewById(R.id.backgroundSplash).setBackground(gradientDrawable);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2500);
+    }
+}
